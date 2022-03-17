770138a175efc76f5060e877824f8149d39df0a2
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2022-03-17 14:21:03 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 .curso-main-container.introduccion
-  BannerInterno.bnr
+  BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5  
     .titulo-principal.color-acento-contenido(data-aos="fade-left")
       .titulo-principal__numero
@@ -11,11 +11,11 @@
     .row.justify-content-center.align-items-center.py-5
       .col-lg-10 
         .cajon-b.color-primario.p-3        
-          p(data-aos="fade-down") La discriminación y el racismo han sido determinantes en la vida de los pueblos indígenas, pues debido a ello se han generado grandes vulneraciones a sus derechos, sobre todo, los más fundamentales. En el siguiente recurso, se podrá conocer sobre el despojos de tierras, cómo han sido víctimas de genocidio y desaparición forzada, temas que serán la introducción a este componente formativo. Además, seguir profundizando en temas como el patrimonio cultural y su lenguaje. Bienvenidos:
+          p(data-aos="fade-down") La discriminación y el racismo han sido determinantes en la vida de los pueblos indígenas, pues debido a ello se han generado grandes vulneraciones a sus derechos, sobre todo, los más fundamentales. En el siguiente recurso, se podrá conocer sobre el despojo de tierras, cómo han sido víctimas de genocidio y desaparición forzada, temas que serán la introducción a este componente formativo. Además, seguir profundizando en temas como el patrimonio cultural y su lenguaje. Bienvenidos:
         
         figure.mt-5(data-aos="fade-left")
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/p_sk9FYRCl4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
 </template>
 
